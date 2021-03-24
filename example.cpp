48399a9f1db596c71dd6ba811f2e55b06fdf9335
chanchann <www.769440972@qq.com>
2021-03-24 11:59:23 +0800

    accept connections, print http request parsed information

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include "flask.h"
+#include <iostream>
+
+flask::Flask app;
+
+int main()
+{
+    app.route("/",
+    []{
+        return "Hello World!";
+    });
+
+    app.port(8080)
+       .run();
+}
---
+++ flask.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <functional>
+#include <memory>
+#include <future>
+#include <stdint.h>
+
+#include "http_server.h"
+
+// TEST
+#include <iostream>
+
+namespace flask
+{
+    class Flask
+    {
+    public:
+        Flask()
+        {
+        }
+
+       void handle()
+        {
+        }
+
+        void route(const std::string& url, std::function<std::string()> f)
+        {
+        }
+
+        Flask& port(std::uint16_t port)
+        {
+            port_ = port;
+            return *this;
+        }
+
+        void run()
+        {
+            Server<Flask> server(this, port_);
+            server.run();
+        }
+    private:
+        uint16_t port_ = 80;
+    };
+};
